2fb97c5bcb246b6dc8f85f5f88425528881a3e31
markoobradovic <marko123obradovic@gmail.com>
2023-10-12 00:22:59 +0200

    installed tailwind + nuxt dev tools

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>My Portfolio</h1>
-    <!-- <NuxtWelcome /> -->
+    <h2 class="text-3xl pl-10 pt-10">Marko Obradovic</h2>
+    <h1 class="text-5xl pl-10 font-semibold">My Porfolio Website</h1>
   </div>
 </template>
