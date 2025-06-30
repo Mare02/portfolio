cba7016869e2e0435431d7010cdb98d28c23dd2c
markoobradovic <marko123obradovic@gmail.com>
2025-06-30 22:26:09 +0200

    added footer

--- app.vue
+++ app.vue
@@ -2,8 +2,10 @@
 <div class="font-ibmsans">
     <Navbar />
     <NuxtPage></NuxtPage>
+    <Footer />
   </div>
 </template>
 <script setup>
-  import Navbar from './components/MainElements/Navbar.vue';
+  import Navbar from '@/components/MainElements/Navbar.vue';
+  import Footer from '@/components/MainElements/Footer.vue';
 </script>
--- components/MainElements/Socials.vue
+++ components/MainElements/Socials.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex gap-4 md:gap-6">
-    <a href="https://github.com/Mare02" target="_blank" class="w-7 md:w-8">
-      <img loading="lazy" src="~/assets/images/socials/github-mark.png" alt="">
+    <a href="https://github.com/Mare02" target="_blank" class="w-7 md:w-8 github-link hover:opacity-80 transition-opacity duration-200">
+      <img loading="lazy" src="~/assets/images/socials/github-mark.png" alt="github icon">
     </a>
-    <a href="https://www.linkedin.com/in/marko-obradovi%C4%87-18b252257/" target="_blank" class="w-7 md:w-8">
-      <img loading="lazy" src="~/assets/images/socials/In-Blue-48.png" alt="">
+    <a href="https://www.linkedin.com/in/marko-obradovi%C4%87-18b252257/" target="_blank" class="w-7 md:w-8 hover:opacity-80 transition-opacity duration-200">
+      <img loading="lazy" src="~/assets/images/socials/In-Blue-48.png" alt="linkedin icon">
     </a>
   </div>
 </template>
