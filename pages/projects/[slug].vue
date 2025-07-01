01ff80778923938fb3c64f8463483359d00d2cc4
markoobradovic <marko123obradovic@gmail.com>
2025-07-02 01:21:04 +0200

    wip

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLayout class="nav-top-padding pb-10">
+  <NuxtLayout class="nav-top-padding pb-20">
     <div class="w-full max-h-72 overflow-hidden flex items-start justify-center rounded-2xl border-2">
       <img
         :src="project.images[0]"
@@ -8,8 +8,27 @@
         loading="lazy"
       >
     </div>
-    <section-title class="mb-6 md:mb-8 mt-8" :title="project.name[locale]"></section-title>
-    <div class="prose dark:text-gray-200">
+    <div class="flex flex-col lg:flex-row lg:items-center lg:gap-20 mb-4 lg:mb-0">
+      <section-title class="mb-4 md:mb-6 mt-8 gradient-text" :title="project.name[locale]"></section-title>
+      <div class="flex items-center gap-4 min-w-max">
+        <a :href="project.website_url" target="_blank" rel="noopener noreferrer" v-if="project.website_url">
+          <Button link class="border-2">
+            <MdiIcon icon="mdiSearchWeb" class="mr-2"/>
+            {{ $t('Website') }}
+          </Button>
+        </a>
+        <a :href="project.source_code_url" target="_blank" rel="noopener noreferrer" v-if="project.source_code_url">
+          <Button link class="border-2">
+            <MdiIcon icon="mdiCodeBraces" class="mr-2"/>
+            {{ $t('Code') }}
+          </Button>
+        </a>
+      </div>
+    </div>
+    <p class="section-pharagraph mb-4 md:mb-6">
+      {{ project.description[locale] }}
+    </p>
+    <div class="prose dark:prose-invert dark:text-gray-200" v-if="project.html[locale]">
       <div v-html="project.html[locale]"></div>
     </div>
   </NuxtLayout>
@@ -18,6 +37,7 @@
 <script setup>
 import SectionTitle from '@/components/MainElements/SectionTitle.vue';
 import projects from '@/data/projects.json';
+import Button from '@/components/UI/Button.vue';
 import { useI18n } from 'vue-i18n';
 const { locale } = useI18n();
 const route = useRoute();
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,45 +1,58 @@
 <template>
-  <div class="">
-    <NuxtLayout class="nav-top-padding pb-10">
-      <section-title class="mb-6 md:mb-8" :title="$t('My projects')"></section-title>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 py-8">
+  <NuxtLayout class="nav-top-padding pb-20">
+    <section-title class="mb-6 md:mb-8" :title="$t('My projects')"></section-title>
+    <div class="flex items-center justify-center gap-2">
+      <Button @click="toggleResourceView('grid')" icon :active="resourceView === 'grid'">
+        <MdiIcon icon="mdiViewGridOutline"/>
+      </Button>
+      <Button @click="toggleResourceView('list')" icon :active="resourceView === 'list'">
+        <MdiIcon icon="mdiFormatListBulleted"/>
+      </Button>
+    </div>
+    <div class="grid grid-cols-1 py-8 gap-6 lg:gap-8" :class="resourceView === 'grid' ? 'md:grid-cols-2' : ''">
       <NuxtLink
-          v-for="project in projects"
-          :key="project.name.en"
-          :to="localePath(`/projects/${project.slug.en}`)"
-          :title="project.name[locale]"
-          class="hover:opacity-80 theme-transition h-full"
-        >
-          <div class="flex flex-col rounded-2xl overflow-hidden border-2 h-full">
-            <div class="aspect-video w-full overflow-hidden">
-              <img
-                :src="project.images[0]"
-                :alt="project.name[locale]"
-                class="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
-              />
-            </div>
-            <div class="p-6 border-t-2 rounded-b-2xl landing-hero-background flex-1">
-              <h2 class="section-pharagraph font-semibold gradient-text">
-                {{ project.name[locale] }}
-              </h2>
-              <p class="section-pharagraph-small">
-                {{ project.description[locale] }}
-              </p>
-            </div>
+        v-for="project in projects"
+        :key="project.name.en"
+        :to="localePath(`/projects/${project.slug.en}`)"
+        :title="project.name[locale]"
+        class="hover:opacity-80 theme-transition h-full"
+      >
+        <div class="flex rounded-2xl overflow-hidden border-2 h-full" :class="resourceView === 'grid' ? 'flex-col' : ''">
+          <div class="aspect-video overflow-hidden" :class="resourceView === 'list' ? 'w-1/3 md:w-1/5' : 'w-full'">
+            <img
+              :src="project.images[0]"
+              :alt="project.name[locale]"
+              class="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+            />
           </div>
-        </NuxtLink>
-      </div>
-    </NuxtLayout>
-  </div>
+          <div class="rounded-b-2xl bg-gray-100 dark:bg-slate-900 flex-1 p-4 md:p-6" :class="resourceView === 'grid' ? 'border-t-2' : ''">
+            <h2 class="section-pharagraph font-semibold gradient-text">
+              {{ project.name[locale] }}
+            </h2>
+            <p class="section-pharagraph-small">
+              {{ project.description[locale] }}
+            </p>
+          </div>
+        </div>
+      </NuxtLink>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script setup>
 import SectionTitle from '@/components/MainElements/SectionTitle.vue';
 import projects from '@/data/projects.json';
 import { useI18n } from 'vue-i18n';
+import Button from '@/components/UI/Button.vue';
 const { t, locale } = useI18n();
+import { ref } from 'vue';
 
 const localePath = useLocalePath();
+
+const resourceView = ref('grid');
+const toggleResourceView = (view) => {
+  resourceView.value = view;
+};
 
 useSeoMeta({
   title: t('seo.index.title'),
