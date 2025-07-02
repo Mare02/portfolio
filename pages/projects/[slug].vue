c9aa805bddfa31b68acc5c9ce994d74060c48349
markoobradovic <marko123obradovic@gmail.com>
2025-07-03 01:34:24 +0200

    added sanity

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,69 +1,7 @@
-<template>
-  <NuxtLayout class="nav-top-padding pb-20">
-    <div
-      class="w-full max-h-72 overflow-hidden flex items-start justify-center rounded-2xl border-2"
-      v-if="project.images[0]"
-    >
-      <img
-        :src="project.images[0]"
-        :alt="project.name[locale]"
-        class="w-full h-auto object-cover"
-        loading="lazy"
-      >
-    </div>
-    <div class="flex flex-col lg:flex-row lg:items-center lg:gap-20 mb-4 lg:mb-0">
-      <section-title class="mb-4 md:mb-6 mt-8 gradient-text" :title="project.name[locale]"></section-title>
-      <div
-        class="flex items-center gap-4 min-w-max"
-        v-if="project.website_url[locale] || project.source_code_url"
-      >
-        <a :href="project.website_url[locale]" target="_blank" rel="noopener noreferrer" v-if="project.website_url[locale]">
-          <Button link class="border-2">
-            <MdiIcon icon="mdiWeb" class="mr-2"/>
-            {{ $t('Website') }}
-          </Button>
-        </a>
-        <a :href="project.source_code_url" target="_blank" rel="noopener noreferrer" v-if="project.source_code_url">
-          <Button link class="border-2">
-            <MdiIcon icon="mdiCodeBraces" class="mr-2"/>
-            {{ $t('Code') }}
-          </Button>
-        </a>
-      </div>
-    </div>
-    <p class="section-pharagraph mb-4 md:mb-6" v-if="project.description[locale]">
-      {{ project.description[locale] }}
-    </p>
-    <div class="py-4" v-if="project.images.length">
-      <h3 class="section-pharagraph mb-3 underline">{{ $t('Project gallery') }}</h3>
-      <lightgallery
-        :settings="{ speed: 500, plugins: [lgZoom] }"
-        class="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-2 md:gap-4"
-      >
-        <a
-          v-for="image in project.images"
-          :key="image"
-          :href="image"
-          class="block aspect-square overflow-hidden rounded-lg border-2"
-        >
-          <img
-            :alt="image"
-            :src="image"
-            class="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
-          />
-        </a>
-      </lightgallery>
-    </div>
-    <div class="prose dark:prose-invert dark:text-gray-200" v-if="project.html[locale]">
-      <div v-html="project.html[locale]"></div>
-    </div>
-  </NuxtLayout>
-</template>
-
 <script setup>
 import SectionTitle from '@/components/MainElements/SectionTitle.vue';
-import projects from '@/data/projects.json';
 import Button from '@/components/UI/Button.vue';
+import imageUrlBuilder from "@sanity/image-url";
 
 import Lightgallery from 'lightgallery/vue';
 import lgZoom from 'lightgallery/plugins/zoom';
@@ -73,19 +11,76 @@ import { useI18n } from 'vue-i18n';
 const { locale } = useI18n();
 const route = useRoute();
 
-const project = projects.find(project => project.slug.en === route.params.slug);
+const { projectId, dataset } = useSanity().client.config();
+const urlFor = (source) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source).url()
+    : null;
+
+const query = groq`*[_type == "project" && slug.en.current == $slug][0]`;
+const { data } = await useSanityQuery(query, { slug: route.params.slug });
+const project = data.value;
 </script>
 
-<style lang="css" scoped>
-/* .lg-gallery img {
-  width: 100%;
-  height: auto;
-  display: block;
-  object-fit: cover;
-}
-.lg-gallery a {
-  display: inline-block;
-  width: 200px;
-  margin: 5px;
-} */
-</style>
+<template>
+  <NuxtLayout class="nav-top-padding pb-20">
+    <div
+      class="w-full max-h-72 overflow-hidden flex items-start justify-center rounded-2xl border-2"
+      v-if="project && project.images[0]"
+    >
+      <img
+        :src="urlFor(project.images[0].asset._ref)"
+        :alt="project.name[locale]"
+        class="w-full h-auto object-cover"
+        loading="lazy"
+      >
+    </div>
+    <div class="flex flex-col lg:flex-row lg:items-center lg:gap-20 mb-4 lg:mb-0" v-if="project && project.name">
+      <section-title class="mb-4 md:mb-6 mt-8 gradient-text" :title="project.name[locale]"></section-title>
+      <div
+        class="flex items-center gap-4 min-w-max"
+        v-if="project.websiteUrl[locale] || project.sourceCodeUrl"
+      >
+        <a :href="project.websiteUrl[locale]" target="_blank" rel="noopener noreferrer" v-if="project.websiteUrl[locale]">
+          <Button link class="border-2">
+            <MdiIcon icon="mdiWeb" class="mr-2"/>
+            {{ $t('Website') }}
+          </Button>
+        </a>
+        <a :href="project.sourceCodeUrl" target="_blank" rel="noopener noreferrer" v-if="project.sourceCodeUrl">
+          <Button link class="border-2">
+            <MdiIcon icon="mdiCodeBraces" class="mr-2"/>
+            {{ $t('Code') }}
+          </Button>
+        </a>
+      </div>
+    </div>
+    <p class="section-pharagraph mb-4 md:mb-6" v-if="project && project.description[locale]">
+      {{ project.description[locale] }}
+    </p>
+    <div class="py-4 mb-4 md:mb-6" v-if="project && project.images && project.images.length">
+      <h3 class="section-pharagraph mb-3 underline">{{ $t('Project gallery') }}</h3>
+      <lightgallery
+        :settings="{ speed: 500, plugins: [lgZoom], selector: 'a' }"
+        class="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-2 md:gap-4"
+      >
+        <a
+          v-for="image in project.images"
+          :key="image.asset._ref"
+          :href="urlFor(image.asset._ref)"
+          :data-src="urlFor(image.asset._ref)"
+          class="block aspect-square overflow-hidden rounded-lg border-2"
+        >
+          <img
+            :alt="image"
+            :src="urlFor(image.asset._ref)"
+            class="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
+          />
+        </a>
+      </lightgallery>
+    </div>
+    <div class="prose dark:prose-invert dark:text-gray-200" v-if="project && project.htmlContent[locale]">
+      <SanityContent :blocks="project.htmlContent[locale]" />
+    </div>
+  </NuxtLayout>
+</template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,3 +1,44 @@
+<script setup>
+import SectionTitle from '@/components/MainElements/SectionTitle.vue';
+import { useI18n } from 'vue-i18n';
+import Button from '@/components/UI/Button.vue';
+const { t, locale } = useI18n();
+import { ref } from 'vue';
+import imageUrlBuilder from "@sanity/image-url";
+
+const localePath = useLocalePath();
+
+const resourceView = ref('grid');
+
+const toggleResourceView = (view) => {
+  resourceView.value = view;
+  if (window && localStorage.getItem('resourceView') !== view) {
+    localStorage.setItem('resourceView', view);
+  }
+};
+
+const { projectId, dataset } = useSanity().client.config();
+const urlFor = (source) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source)
+    : null;
+
+onMounted(() => {
+  if (window && localStorage) {
+    resourceView.value = localStorage.getItem('resourceView') || 'grid';
+  }
+});
+
+useSeoMeta({
+  title: t('seo.index.title'),
+  description: t('seo.index.description'),
+});
+
+const query = groq`*[_type == "project"]`;
+const { data } = await useSanityQuery(query);
+const projects = data.value;
+</script>
+
 <template>
   <NuxtLayout class="nav-top-padding pb-20">
     <section-title class="mb-6 md:mb-8" :title="$t('My projects')"></section-title>
@@ -9,11 +50,14 @@
         <MdiIcon icon="mdiFormatListBulleted"/>
       </Button>
     </div>
-    <div class="grid py-8 gap-6 lg:gap-8" :class="resourceView === 'grid' ? 'md:grid-cols-2' : ''">
+    <div
+      v-if="projects && projects.length"
+      class="grid py-8 gap-6 lg:gap-8" :class="resourceView === 'grid' ? 'md:grid-cols-2' : ''"
+    >
       <NuxtLink
         v-for="project in projects"
-        :key="project.name.en"
-        :to="localePath(`/projects/${project.slug.en}`)"
+        :key="project._id"
+        :to="localePath(`/projects/${project.slug.en.current}`)"
         :title="project.name[locale]"
         class="hover:opacity-80 theme-transition h-full"
       >
@@ -23,7 +67,7 @@
             :class="resourceView === 'list' ? 'w-1/2 md:w-1/4 md:aspect-video' : 'w-full aspect-video'"
           >
             <img
-              :src="project.images[0]"
+              :src="urlFor(project.images[0].asset._ref)"
               :alt="project.name[locale]"
               class="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
             />
@@ -42,8 +86,8 @@
               <div class="flex items-start gap-1">
                 <a
                   @click.stop=""
-                  v-if="project.website_url[locale]"
-                  :href="project.website_url[locale]"
+                  v-if="project.websiteUrl[locale]"
+                  :href="project.websiteUrl[locale]"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -53,8 +97,8 @@
                 </a>
                 <a
                   @click.stop=""
-                  v-if="project.source_code_url"
-                  :href="project.source_code_url"
+                  v-if="project.sourceCodeUrl"
+                  :href="project.sourceCodeUrl"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -73,34 +117,3 @@
     </div>
   </NuxtLayout>
 </template>
-
-<script setup>
-import SectionTitle from '@/components/MainElements/SectionTitle.vue';
-import projects from '@/data/projects.json';
-import { useI18n } from 'vue-i18n';
-import Button from '@/components/UI/Button.vue';
-const { t, locale } = useI18n();
-import { ref } from 'vue';
-
-const localePath = useLocalePath();
-
-const resourceView = ref('grid');
-
-const toggleResourceView = (view) => {
-  resourceView.value = view;
-  if (window && localStorage.getItem('resourceView') !== view) {
-    localStorage.setItem('resourceView', view);
-  }
-};
-
-onMounted(() => {
-  if (window && localStorage) {
-    resourceView.value = localStorage.getItem('resourceView') || 'grid';
-  }
-});
-
-useSeoMeta({
-  title: t('seo.index.title'),
-  description: t('seo.index.description'),
-});
-</script>
